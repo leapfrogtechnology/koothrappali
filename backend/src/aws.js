ae827ef8888cc9ba9b3cbedffc1d2311a6df5d94
Samir Poudel <samir@samirpdl.com.np>
2018-11-26 14:50:01 +0545

    Update aws.js for removing unnecessary regions for s3

--- backend/src/aws.js
+++ backend/src/aws.js
@@ -37,20 +37,7 @@ const awsConfig = {
     'sa-east-1': new AWS.RDS({ apiVersion: awsApiVersion, region: 'sa-east-1' }) // Sao Paulo
   },
   s3: {
-    'us-east-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'us-east-1' }), // N. Virginia
-    'us-east-2': new AWS.S3({ apiVersion: awsApiVersion, region: 'us-east-2' }), // Ohio
-    'us-west-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'us-west-1' }), // N. California
-    'us-west-2': new AWS.S3({ apiVersion: awsApiVersion, region: 'us-west-2' }), // Oregon
-    'ca-central-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'ca-central-1' }), // Canada (Central)
-    'eu-west-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'eu-west-1' }), // Ireland
-    'eu-central-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'eu-central-1' }), // Frankfurt
-    'eu-west-2': new AWS.S3({ apiVersion: awsApiVersion, region: 'eu-west-2' }), // London
-    'ap-northeast-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'ap-northeast-1' }), // Tokyo
-    'ap-northeast-2': new AWS.S3({ apiVersion: awsApiVersion, region: 'ap-northeast-2' }), // Seoul
-    'ap-southeast-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'ap-southeast-1' }), // Singapore
-    'ap-southeast-2': new AWS.S3({ apiVersion: awsApiVersion, region: 'ap-southeast-2' }), // Syndney
-    'ap-south-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'ap-south-1' }), // Mumbai
-    'sa-east-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'sa-east-1' }) // Sao Paulo
+    'us-east-1': new AWS.S3({ apiVersion: awsApiVersion, region: 'us-east-1' })
   }
 };
 
