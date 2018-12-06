bd44dce441b776f649183f526b2015a956b55c0f
Samir Poudel <samir@samirpdl.com.np>
2018-12-06 11:07:00 +0545

    Add line in tags.js after const

--- backend/src/utils/tags.js
+++ backend/src/utils/tags.js
@@ -14,6 +14,7 @@ const TAG_DEPLOYMENT = 'Deployment';
  */
 export function assignUsingTags(tags) {
   const instance = {};
+
   tags.forEach(tag => {
     switch (tag.Key) {
       case TAG_PROJECT:
